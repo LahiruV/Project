07845187a279650acdf68ee7f074d3887ed03496
lahirupeer <lahirupeer@gmail.com>
2024-05-16 03:15:33 +0000

    add

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,39 @@
-import express from 'express';
+import express, { Application } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+dotenv.config();
 
-const app = express();
+const app: Application = express();
+const port: number | string = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use(cors());
+app.use(express.json());
+
+const url: string | undefined = process.env.ATLAS_URI;
+if (!url) {
+  console.error("MongoDB connection URI not provided");
+  process.exit(1);
+}
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connection successful");
+}).catch((error) => {
+  console.error("MongoDB connection error:", error);
+  process.exit(1);
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB connection successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
